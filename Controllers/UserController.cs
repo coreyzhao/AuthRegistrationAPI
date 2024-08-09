eeccc3a1cd62bd96c04389b05c76271163d72eb9
coreyzhao <coreyzh2004@gmail.com>
2024-08-09 08:02:19 -0400

    ENH: get_users (single) endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -17,15 +17,38 @@ public class UserController : ControllerBase
     public DateTime TestConnection() {
         return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
     }
-    
-    [HttpGet("GetUsers/{testValue}")]
-    public string[] GetUsers(string testValue)
+
+
+
+    [HttpGet("GetUsers")]
+    public IEnumerable<User> GetUsers()
     {
-        string[] responseArray = new string[] {
-            "test1",
-            "test2",
-            testValue
-        };
-        return responseArray;
+        string sql = @"
+            SELECT  [UserId]
+            , [FirstName]
+            , [LastName]
+            , [Email]
+            , [Gender]
+            , [Active]
+            FROM  TutorialAppSchema.Users";
+
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        return users;
+    }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    public User GetSingleUser(int userId)
+    {
+        string sql = @"
+            SELECT [UserId],
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active] 
+            FROM TutorialAppSchema.Users
+                WHERE UserId = " + userId.ToString(); //"7"
+        User user = _dapper.LoadDataSingle<User>(sql);
+        return user;
     }
 }
